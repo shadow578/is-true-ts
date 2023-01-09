ed71c7acc745c021189798e62e46e909d0d837bd
shadow578 <52449218+shadow578@users.noreply.github.com>
2023-01-09 16:24:27 +0100

    auto-format code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 /**
  * check if a boolean is true
- * 
- * @param value the value to check 
+ *
+ * @param value the value to check
  * @returns is the value true and a boolean
  */
 export function isTrue(value: boolean): boolean {
-    return typeof (value) === "boolean" && !!value;
+  return typeof value === 'boolean' && !!value;
 }
